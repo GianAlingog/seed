503b79c7cdc047410d8baab5a33a2140d663e50e
GianAlingog <gianqalingog@gmail.com>
2024-03-09 23:06:20 -0800

    appbar added

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -6,10 +7,24 @@ import LevelSelectScreen from "./screens/LevelSelectScreen";
 
 const Stack = createNativeStackNavigator();
 
+function HeaderLogo() {
+  return (
+    <Image
+      style={{ width: 150, height: 40 }}
+      source={require("./assets/icons/seed.png")}
+    />
+  );
+}
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerTitle: (props) => <HeaderLogo {...props} />,
+          headerTitleAlign: "center",
+        }}
+      >
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Levels" component={LevelSelectScreen} />
       </Stack.Navigator>
